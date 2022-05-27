801e695adcebcfc0ddd2eeedccf51e0a6f29a7fb
TerrificTable55 <omegagaming189gmail.com>
2022-05-28 00:30:57 +0200

    javadoc files

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"xyz.terrific.bot"},{"l":"xyz.terrific.bot.command"},{"l":"xyz.terrific.bot.command.commands"},{"l":"xyz.terrific.client"},{"l":"xyz.terrific.server"},{"l":"xyz.terrific.server.bot.cnc"},{"l":"xyz.terrific.server.client.cnc"}];updateSearchResults();
